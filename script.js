df63f1cd6e6094bd5e38d63594603eb5d2b6261e
marjanovic-zeljko <101601784+marjanovic-zeljko@users.noreply.github.com>
2023-05-16 11:51:41 +0200

    Fixed animation and image zoom

--- script.js
+++ script.js
@@ -86,29 +86,29 @@ function myFunction() {
 }
 // sticky navigation end here
 
-// // zoom on click start
-// window.onload = () => {
-//     // (A) GET ALL IMAGES
-//     let all = document.getElementsByClassName("zoomE");
+// zoom on click start
+window.onload = () => {
+    // (A) GET ALL IMAGES
+    let all = document.getElementsByClassName("zoomE");
 
-//     // (B) CLICK TO GO FULLSCREEN
-//     if (all.length > 0) {
-//         for (let i of all) {
-//             i.onclick = () => {
-//                 // (B1) EXIT FULLSCREEN
-//                 if (document.fullscreenElement != null || document.webkitFullscreenElement != null) {
-//                     if (document.exitFullscreen) { document.exitFullscreen(); } else { document.webkitCancelFullScreen(); }
-//                 }
+    // (B) CLICK TO GO FULLSCREEN
+    if (all.length > 0) {
+        for (let i of all) {
+            i.onclick = () => {
+                // (B1) EXIT FULLSCREEN
+                if (document.fullscreenElement != null || document.webkitFullscreenElement != null) {
+                    if (document.exitFullscreen) { document.exitFullscreen(); } else { document.webkitCancelFullScreen(); }
+                }
 
-//                 // (B2) ENTER FULLSCREEN
-//                 else {
-//                     if (i.requestFullscreen) { i.requestFullscreen(); } else { i.webkitRequestFullScreen(); }
-//                 }
-//             };
-//         }
-//     }
-// };
-// // zoom on click end
+                // (B2) ENTER FULLSCREEN
+                else {
+                    if (i.requestFullscreen) { i.requestFullscreen(); } else { i.webkitRequestFullScreen(); }
+                }
+            };
+        }
+    }
+};
+// zoom on click end
 
 // animation on scroll start
 function revealSections() {
